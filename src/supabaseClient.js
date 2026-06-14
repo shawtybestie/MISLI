66f48722e615e966c0eb17dc45c84de27fd18adc
shawtybestie <oldbegemotbeats@gmail.com>
2026-06-14 05:12:26 -0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,5 @@
-import { createClient } from '@supabase/supabase-client'
+import { createClient } from '@supabase/supabase-js' 
 
-// Теперь Vite будет безопасно брать их из файла .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
